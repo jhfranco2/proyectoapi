95ff6b5cf2286fd21c0034698c89c4a8840c1aec
jhfranco2 <jhoanmateofranco@gmail.com>
2020-09-20 18:59:39 -0500

    agregue barra

--- apipokemon/src/app/app.module.ts
+++ apipokemon/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraComponent } from './Barra/barra/barra.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraComponent
   ],
   imports: [
     BrowserModule,
