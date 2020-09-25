7796220c8ac8e2ae8ad2f07832baf470fed0b5d8
fasaenzg <70648217+fasaenzg@users.noreply.github.com>
2020-09-24 21:25:14 -0500

    boton y logo

--- apipokemon/src/app/app.component.ts
+++ apipokemon/src/app/app.component.ts
@@ -7,15 +7,19 @@ import { ConexionService } from './Service/conexion.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ApiPokemon';
-  public pokemon:Array<any>=[]
+  title = 'apipokemon';
+  public pokemon:any
+  public hola:String= "equis";
+  public stats:Array<any>;
   constructor(
-    private ConexionService:ConexionService 
+    private ConexionService:ConexionService
   ){
     
     this.ConexionService.getConexion().subscribe((resp:any)=>{
       
       this.pokemon = resp
+      this.stats = resp.stats
+      console.log(this.stats)
     })
   }
 }
